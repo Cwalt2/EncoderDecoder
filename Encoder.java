aa84dff99b0410262a3352192fbd0cf93e219c27
Cwalt2 <98202591+Cwalt2@users.noreply.github.com>
2023-05-10 00:00:01 -0500

    Update Encoder.java

--- Encoder.java
+++ Encoder.java
@@ -1,13 +1,13 @@
-public class Decoder {
-    public static String decode(int[] input) {
-        return decode(input, 0);
+public class Encoder {
+      public static int[] encode(String input) {
+        return encode(input, 0);
     }
 
-    public static String decode(int[] input, int offset) {
-        String decoded = "";
-        for (int i = 0; i < input.length; i++) {
-            decoded += (char) (input[i] - offset);
+    public static int[] encode(String input, int offset) {
+        int[] encoded = new int[input.length()];
+        for (int i = 0; i < input.length(); i++) {
+            encoded[i] = input.charAt(i) + offset;
         }
-        return decoded; // returns decoded
+        return encoded; // returns encoded
     }
 }
